7de6cbda7bbe5cd2eea132ca65f9209763f2ee36
Ece Deniz <ecedenniz@gmail.com>
2019-07-25 21:06:00 +0300

    rearranged audio.m

--- audio.m
+++ audio.m
@@ -1,17 +1,20 @@
-[data, fs] = audioread('audio_oboe.wav');        %reading the audio as a wave function
-data_fft = fft(data);                            %changing domain from time to frequency with fast forier transfrom   
+[data, fs] = audioread('audio_oboe.wav');         %reading the audio as a wave function (.wav)
+data_fft   = fft(data);                           %changing domain from time to frequency with fast forier transfrom in order to observe the frequency that causes the background noise
+L = length(data);                                 %storing length of the data to variable L
+
+figure;                                           %opening a figure
+subplot(2,1,1); plot(abs(data_fft(:,1)));         %the first graph, not filtered. Using abs because negative frequencies do not exist (negative values for time is not proved yet)
+title('Amplitude Spectrum of the Audio ');
+xlabel('f[Hz]');
+ylabel('Amplitude');
+
+%after observing the cut-off frequency, use it to get rid of the background noise
+
 low_filter = lowpass((data_fft), 1.18*exp(5),fs); %filtering the frequencies above 1.18*exp(5) Hz)
 
-L = length(data);  
-figure;                                          %opening a figure                                         
-subplot(2,1,1); plot(abs(data_fft(:,1)));        %
-title('Amplitude Spectrum of the Audio ');       %
-xlabel('f[Hz]');                                 %
-ylabel('Amplitude');                             %
+subplot(2,1,2); plot(abs(low_filter(:,1)));       %the second graph, filtered
+title('Lowpass filter with cutoff frequency of 118kHz ');
+xlabel('f[Hz]');
+ylabel('Amplitude');
 
-subplot(2,1,2); plot(abs(low_filter(:,1)));      %
-title('Lowpass filter with cutoff frequency of 118kHz '); %
-xlabel('f[Hz]');                                 %
-ylabel('Amplitude');                             %
-
-soundsc(low_filter, fs);                         %
+soundsc(low_filter, fs);                          %playing the filtered audio
